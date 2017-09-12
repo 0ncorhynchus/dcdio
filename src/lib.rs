9305ea98c196dfa82531db5b5f6fabfc470bf04e
Suguru Kato <salmon.bioinformatics@gmail.com>
2017-09-12 17:14:13 +0900

    Move the DcdHeader::load() to read_header()

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,9 @@ mod unformatted;
 mod error;
 mod read;
 
-use std::io::{Read, Cursor, SeekFrom};
-use std::io::prelude::*;
-use byteorder::*;
-use unformatted::ReadUnformattedExt;
-use error::*;
-
 pub use read::{DcdReader, DcdFrames};
 
-type Endian = NativeEndian;
+type Endian = byteorder::NativeEndian;
 type Position = (f32, f32, f32);
 
 pub struct Frame {
@@ -41,54 +35,3 @@ pub struct DcdHeader {
     pub title: String
 }
 
-impl DcdHeader {
-    pub fn load<R: Read + ?Sized> (reader: &mut R) -> Result<Self> {
-        let mut buf = Cursor::new(reader.read_unformatted::<Endian>()?);
-        buf.seek(SeekFrom::Current(4))?;
-
-        let num_frames    = buf.read_i32::<Endian>()? as usize;
-        let initial_step  = buf.read_i32::<Endian>()?;
-        let step_interval = buf.read_i32::<Endian>()?;
-
-        buf.seek(SeekFrom::Current(20))?;
-
-        let num_fixed_atoms = buf.read_i32::<Endian>()? as usize;
-        if num_fixed_atoms != 0 {
-            return Err(Error::NotSupported(
-                    "Fixed atoms are not supported.".to_string()));
-        }
-
-        let delta = buf.read_f32::<Endian>()?;
-
-        buf.seek(SeekFrom::Current(36))?;
-
-        let version = buf.read_i32::<Endian>()?;
-
-        buf = Cursor::new(reader.read_unformatted::<Endian>()?);
-        let num_titles = buf.read_i32::<Endian>()? as usize;
-        let mut lines = vec![0u8; num_titles * 80];
-        buf.read(&mut lines)?;
-        let title = String::from_utf8(lines)?;
-
-        buf = Cursor::new(reader.read_unformatted::<Endian>()?);
-        let num_atoms = buf.read_i32::<Endian>()? as usize;
-
-        Ok(DcdHeader {
-            num_frames:     num_frames,
-            initial_step:   initial_step,
-            step_interval:  step_interval,
-            num_fixed_atoms: num_fixed_atoms,
-            delta:           delta,
-            version:         version,
-            num_atoms:       num_atoms,
-            title:           title,
-        })
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
--- src/read.rs
+++ src/read.rs
@@ -1,4 +1,4 @@
-use std::io::{Read, Cursor};
+use std::io::{Read, Cursor, Seek, SeekFrom};
 use unformatted::ReadUnformattedExt;
 use byteorder::ReadBytesExt;
 use ::error::*;
@@ -13,7 +13,7 @@ pub struct DcdReader<R> {
 
 impl<R: Read> DcdReader<R> {
     pub fn new(mut reader: R) -> Result<Self> {
-        let header = DcdHeader::load(&mut reader)?;
+        let header = read_header(&mut reader)?;
         // let fixed_atoms = vec![(0.0, 0.0, 0.0); header.num_fixed_atoms];
         Ok(DcdReader {
             inner: reader,
@@ -24,14 +24,12 @@ impl<R: Read> DcdReader<R> {
     }
 
     pub fn read_frame(&mut self) -> Result<Frame> {
-        let num_atoms = self.header.num_atoms;
-
         let mut bufx = Cursor::new(self.inner.read_unformatted::<Endian>()?);
         let mut bufy = Cursor::new(self.inner.read_unformatted::<Endian>()?);
         let mut bufz = Cursor::new(self.inner.read_unformatted::<Endian>()?);
 
         let mut positions = Vec::new();
-        for _ in 0..num_atoms {
+        for _ in 0..self.header.num_atoms {
             positions.push((bufx.read_f32::<Endian>()?,
                             bufy.read_f32::<Endian>()?,
                             bufz.read_f32::<Endian>()?));
@@ -39,9 +37,10 @@ impl<R: Read> DcdReader<R> {
 
         let step = self.header.step_interval * self.index as i32
                    + self.header.initial_step;
+        let time = self.header.delta * step as f32;
         self.index += 1;
 
-        Ok(Frame::new(step, self.header.delta * step as f32, positions))
+        Ok(Frame::new(step, time, positions))
     }
 
     pub fn frames(self) -> DcdFrames<R> {
@@ -49,6 +48,49 @@ impl<R: Read> DcdReader<R> {
             reader: self
         }
     }
+}
+
+fn read_header<R: Read + ?Sized>(reader: &mut R) -> Result<DcdHeader> {
+    let mut buf = Cursor::new(reader.read_unformatted::<Endian>()?);
+    buf.seek(SeekFrom::Current(4))?;
+
+    let num_frames    = buf.read_i32::<Endian>()? as usize;
+    let initial_step  = buf.read_i32::<Endian>()?;
+    let step_interval = buf.read_i32::<Endian>()?;
+
+    buf.seek(SeekFrom::Current(20))?;
+
+    let num_fixed_atoms = buf.read_i32::<Endian>()? as usize;
+    if num_fixed_atoms != 0 {
+        return Err(Error::NotSupported(
+                "Fixed atoms are not supported.".to_string()));
+    }
+
+    let delta = buf.read_f32::<Endian>()?;
+
+    buf.seek(SeekFrom::Current(36))?;
+
+    let version = buf.read_i32::<Endian>()?;
+
+    buf = Cursor::new(reader.read_unformatted::<Endian>()?);
+    let num_titles = buf.read_i32::<Endian>()? as usize;
+    let mut lines = vec![0u8; num_titles * 80];
+    buf.read(&mut lines)?;
+    let title = String::from_utf8(lines)?;
+
+    buf = Cursor::new(reader.read_unformatted::<Endian>()?);
+    let num_atoms = buf.read_i32::<Endian>()? as usize;
+
+    Ok(DcdHeader {
+        num_frames:     num_frames,
+        initial_step:   initial_step,
+        step_interval:  step_interval,
+        num_fixed_atoms: num_fixed_atoms,
+        delta:           delta,
+        version:         version,
+        num_atoms:       num_atoms,
+        title:           title,
+    })
 }
 
 pub struct DcdFrames<R> {
